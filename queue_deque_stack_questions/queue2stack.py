554a885d5264d0c66b6d9eb1c469a73f07ff4cb4
Tomoka64 <y.tomoka64@gmail.com>
2018-09-02 23:22:20 +0900

    add queue

---
+++ queue_deque_stack_questions/queue2stack.py
@@ -0,0 +1,18 @@
+class Queue2Stacks(object):
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+    def enqueue(self, item):
+        self.stack1.append(item)
+    def dequeue(self):
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()  
+
+q = Queue2Stacks()
+
+for i in range(5):
+    q.enqueue(i)
+    
+for i in range(5):
+    print(q.dequeue())
