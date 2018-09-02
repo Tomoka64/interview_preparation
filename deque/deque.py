c3f92160bbc30ee46db6bf00f70871b8bd0b30a9
Tomoka64 <y.tomoka64@gmail.com>
2018-09-02 21:36:57 +0900

    add queue

---
+++ deque/deque.py
@@ -0,0 +1,27 @@
+class Deque(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+    
+    def addRear(self, item):
+        self.items.insert(0, item)
+    
+    def removeFront(self):
+        return self.items.pop()
+    
+    def removeRear(self):
+        return self.items.pop(0)
+    
+    def size(self):
+        return len(self.items)
+
+d = Deque()
+print(d.isEmpty())
+d.addFront('hello')
+d.addRear('world')
+print(d.removeFront() + " " + d.removeRear())
